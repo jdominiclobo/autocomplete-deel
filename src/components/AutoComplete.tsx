890d67832be239788ff30bcaefd48bfad0a17101
jdominiclobo <jdominiclobo@gmail.com>
2023-07-28 20:13:22 +0530

    async function for the REST like call

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,5 +1,13 @@
-import React, { useState } from "react";
-import { Option } from "../data/countries";
+import React, { useEffect, useState } from "react";
+import { countries as mockOptions, Option } from "../data/countries";
+
+const fetchOptions = (): Promise<Option[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(mockOptions);
+    }, 1000);
+  });
+};
 
 const AutoComplete: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
